95c812a1c5200e6b1b8db432997ba412806e4478
Yves Permentier <yves.permentier@gmail.com>
2018-07-31 14:07:42 +0200

    fix: Check existance of markers prior to use

--- js/main.js
+++ js/main.js
@@ -112,7 +112,9 @@ resetRestaurants = (restaurants) => {
   ul.innerHTML = '';
 
   // Remove all map markers
-  self.markers.forEach(m => m.setMap(null));
+  if(self.markers){
+    self.markers.forEach(m => m.setMap(null));
+  }
   self.markers = [];
   self.restaurants = restaurants;
 }
